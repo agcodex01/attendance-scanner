c07543afeb1de62d6f1a4a3d53070af2dfc6f7dc
loremZyn14 <yasuaimo@gmail.com>
2022-01-21 20:45:11 +0800

    Add Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +15,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@email.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'avatar' => 'storage/default.gif',
+            'qrcode' => 'nothing',
+            'position' => 'it',
+            'type' => 'admin',
+            'status' => 'active'
+        ]);
     }
 }
